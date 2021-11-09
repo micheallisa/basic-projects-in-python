b462638f7467177c4353fb237b3a07f77bfc03fd
micheallisa <85683520+micheallisa@users.noreply.github.com>
2021-11-09 17:22:03 -0500

    dice.py

--- dice.py
+++ dice.py
@@ -1,11 +1,12 @@
 import random
 import time
 
-print("\n This will roll a 6-sided dice")
+print("\n This will roll a  dice")
 time.sleep(2)
 how_many_times_to_roll = int(input("How many time do you want  the dice to roll: "))
+how_many_sides = int(input("How many sides will the die have? "))
 x = 1
 while x != how_many_times_to_roll:
-    number = random.randint(1, 6)
+    number = random.randint(1, how_many_sides)
     print(number)
     x = x + 1
