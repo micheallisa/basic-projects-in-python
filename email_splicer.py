3f3099655423d1ae77871e292490b13142f2446f
micheallisa <85683520+micheallisa@users.noreply.github.com>
2021-11-09 17:44:47 -0500

    Delete email_splicer.py

--- email_splicer.py
+++
@@ -1,6 +0,0 @@
-email = input("What is your email? ").strip()
-
-username = email[:email.index('@')]
-domain = email[email.index('@') + 1:]
-
-print(f"Your username is {username} and the domain is {domain}")
