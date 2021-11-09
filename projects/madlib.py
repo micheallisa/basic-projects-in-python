f7e6c458a39aea67b502c598ffd2ecdcc7ba417c
micheallisa <85683520+micheallisa@users.noreply.github.com>
2021-11-09 18:47:12 -0500

    Create madlib.py
    readability, whats that?

---
+++ projects/madlib.py
@@ -0,0 +1,27 @@
+import time
+
+
+number1 = input("Please enter a number : ")
+plural_noun = input("Please say a plural noun : ")
+verb = input("Please say a verb ")
+adjective = input("Please say an adjective : ")
+number2 = input("Please say another number : ")
+place = input("Please say a place: ")
+first_name = input("Please say a first name: ")
+first_name2 = input("Please say another first name: ")
+verb_ed = input("Say a past tense verb: ")
+noun = input("Please say a noun: ")
+verb_ed1 = input("Please say a past tense verb: ")
+number3 = input("Please say another number: ")
+adjective2 = input("Please say an adjective: ")
+adjective3 = input("Please say another adjective: ")
+verb_ed2 = input("Please say a past tense verb: ")
+verb_ed3 = input("Please say another past tense verb: ")
+noun1 = input("Please enter a noun: ")
+verb_ed4 = input("Enter another past tense verb: ")
+plural_noun1 = input("Enter a plural noun: ")
+verb1 = input("Please say a verb: ")
+adverb = input("Please say an adverb: ")
+number4 = input("Please say a number: ")
+
+print(f"On July {number1} 1969, two American \n {plural_noun} were the first to {verb} on \n the moon. This {adjective} trip took \n {number2} days to reach the moon from \n {place}. As {first_name} Armstrong \n and {first_name2} Aldrin {verb_ed} \n onto the {noun} of the moon, Armstrong \n {verb_ed1} the famous words, \" That's {number3} \n {adjective2} step for man, one {adjective3} leap for mankind\". Soon after, Aldrin {verb_ed2} onto  \n the moon and together they {verb_ed3} a \n U.S {noun1} on the surface. They {verb_ed4} \n {plural_noun1} from the moon's surface to {verb1} \n back to Earth and {adverb} returned home \n {number4} days later.")
